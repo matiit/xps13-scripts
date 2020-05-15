08c5513d33a09d1e1795a4127bdc55d573654542
Mateusz Tracz <matiqing@gmail.com>
2020-05-15 21:10:43 +0100

    fix input/xinput typo and add finding the correct device id as it changes depending on connected devices

--- touchpad-naturalscrolling.sh
+++ touchpad-naturalscrolling.sh
@@ -3,5 +3,9 @@
 # XFCE4 is lacking a setting option for
 # touchpad's natural scrolling
 # This works like a charm
-input set-prop 13 "libinput Natural Scrolling Enabled" 1
+
+# found the device id
+TOUCHPAD_ID=$(xinput list | grep Touchpad |  grep -o "id=[0-9]*" | grep -o "[0-9]*")
+
+xinput set-prop $TOUCHPAD_ID "libinput Natural Scrolling Enabled" 1
 
