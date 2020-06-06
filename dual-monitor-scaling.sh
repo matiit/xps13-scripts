59ddb34218adc3ceb4308c1e54a780e4d8d5652b
Mateusz Tracz <matiqing@gmail.com>
2020-06-06 13:15:56 +0100

    do not use xrandr --listmonitors - it doesn't work well with some thunderbolt 3 port replicators.

--- dual-monitor-scaling.sh
+++ dual-monitor-scaling.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Just assume it's the only connected external display
-NEWMONITOR=$(xrandr --listmonitors | grep -v eDP-1 | awk '{print $4}')
+NEWMONITOR=$(xrandr | grep ' connected' | grep -v eDP-1 | awk '{print $1}')
 # Use first arg if provided
 OUTPUT="${1:-$NEWMONITOR}"
+
 xrandr --output $OUTPUT --auto --scale 2x2 --right-of eDP-1 || echo "This script only supports one external display, more detected: $NEWMONITOR"
 
 
